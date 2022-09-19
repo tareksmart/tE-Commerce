b987ae40f76bda1bc333b1af4829635d9f6f32c2
tareksmart <tareksmart2015@gmail.com>
2022-09-19 22:22:41 +0200

    adding data user

--- lib/models/user_data.dart
+++ lib/models/user_data.dart
@@ -2,7 +2,19 @@ class UserData{
   final String uid;
   final String email;
 
-  UserData(this.uid, this.email);
+  UserData({required this.uid,required this.email});
 
+  Map<String, dynamic> toMap() {
+    return {
+      'uid': this.uid,
+      'email': this.email,
+    };
+  }
 
+  factory UserData.fromMap(Map<String, dynamic> map) {
+    return UserData(
+      uid: map['uid']?? '',
+      email: map['email'] ?? '',
+    );
+  }
 }
--- lib/services/database_controller.dart
+++ lib/services/database_controller.dart
@@ -1,11 +1,13 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:ecommerce/models/product.dart';
+import 'package:ecommerce/models/user_data.dart';
 import 'package:ecommerce/services/firestore_services.dart';
 import 'package:ecommerce/utilities/api_path.dart';
 
 abstract class Database {
   Stream<List<Product>> salesProductStream();
   Stream<List<Product>> newProductStream();
+  Future<void> setDataUser(UserData userData);
 }
 
 class FireStorDatabase implements Database {
@@ -27,5 +29,10 @@ class FireStorDatabase implements Database {
         path: ApiPath.products,
         builder: (data, documentId) => Product.fromMap(data!, documentId),
         queryBuilder: (query)=>query.where('discountValue',isEqualTo: 0));
+  }
+
+  @override
+  Future<void> setDataUser(UserData userData) async{
+  _service.setData(path: ApiPath.user(userId), data: userData.toMap());
   }
 }
